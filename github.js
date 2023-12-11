9a3ed7c2941267b6fd0832214c662c665a1f20de
Musa <Musaxlilov29@gmail.com>
2023-12-11 14:14:35 +0400

    Minor changes

--- github.js
+++ github.js
@@ -1,1 +1,7 @@
-//alert("ig: @musaxelilovv");
+let starButton = document.querySelector(".repo-stars-container");
+
+starButton.addEventListener("click", showTheIgAcc);
+
+function showTheIgAcc(){
+    //alert("Ziya gotdu");
+}
